d4ef9cd50e17b52297151ed901f51407ac7bd994
RonHabaron <barakbm9@gmail.com>
2026-02-14 18:27:30 +0200

    implemented client and server protocol. next, clientLogic and then serverLogic

--- client/clientProtocol.py
+++ client/clientProtocol.py
@@ -10,6 +10,80 @@ def build_command(command, params):
     return str(command) + "@#".join(params)
 
 
+def build_sign_up(username, password, email):
+    return build_command(0, [username, password, email])
+
+
+def build_sign_in(username_or_email, password):
+    return build_command(1, [username_or_email, password])
+
+
+def build_set_topics(topics):
+    return build_command(2, topics)
+
+
+def build_set_filter(topics):
+    return build_command(3, topics)
+
+
+def build_search_creators(creator_name):
+    return build_command(4, [creator_name])
+
+
+def build_search_videos(video_name_or_desc, topics):
+    return build_command(5, [video_name_or_desc, topics])
+
+
+def build_comment(video_id, comment):
+    return build_command(6, [video_id, comment])
+
+
+def build_ask_test(video_id):
+    return build_command(7, [video_id])
+
+
+def build_report(video_or_comment_id):
+    return build_command(8, [video_or_comment_id])
+
+
+def build_req_comments(video_id, comment_id):
+    return build_command(9, [video_id, comment_id])
+
+
+def build_del_video(video_id):
+    return build_command(10, [video_id])
+
+
+def build_del_comment(comment_id):
+    return build_command(11, [comment_id])
+
+
+def build_req_creator_videos(username):
+    return build_command(12, [username])
+
+
+def build_req_creator_follow(username, follow_type):
+    return build_command(13, [username, follow_type])
+
+
+def build_req_video(video_id):
+    return build_command(14, [video_id])
+
+
+def build_video_detail(video_name, video_desc, test_link):
+    return build_command(15, [video_name, video_desc, test_link])
+
+
+def build_follow_req(username):
+    return build_command(16, [username])
+
+
+# ----- Video transfer protocol -----
+
+def build_file_details(file_name, file_size):
+    return build_command(00, [file_name, file_size])
+
+
 def unpack(data):
     """Unpack a command string into opcode and parameters.
 
--- server/serverProtocol.py
+++ server/serverProtocol.py
@@ -9,6 +9,84 @@ def build_command(command, params):
     """
     return str(command) + "@#".join(params)
 
+
+def build_sign_up_status(status):
+    return build_command(0, [status])
+
+
+def build_sign_in_status(status):
+    return build_command(1, [status])
+
+
+def build_set_topics_confirmation(status):
+    return build_command(2, [status])
+
+
+def build_set_filter_confirmation(status):
+    return build_command(3, [status])
+
+
+def build_creator_details(username, followers_amount, following_amount, videos_amount):
+    return build_command(
+        4,
+        [username, followers_amount, following_amount, videos_amount]
+    )
+
+
+def build_video_details(video_id, creator_name, video_name, video_desc, likes_amount, comments_amount):
+    return build_command(
+        5,
+        [video_id, creator_name, video_name, video_desc, likes_amount, comments_amount]
+    )
+
+
+def build_comment_status(status):
+    return build_command(6, [status])
+
+
+def build_send_test(link):
+    return build_command(7, [link])
+
+
+def build_report_status(status):
+    return build_command(8, [status])
+
+
+def Build_send_comments(comments):
+    # comments = [[comment_id, commenter_name, comment], ...]
+    return build_command(9, comments)
+
+
+def build_del_video_confirmation(status):
+    return build_command(10, [status])
+
+
+def build_del_comment_confirmation(status):
+    return build_command(11, [status])
+
+
+#12-14 are client requests that are handles using other commands
+
+
+def build_video_upload_confirmation(status):
+    return build_command(15, [status])
+
+def build_follow_user_status(status):
+    return build_command(16, [status])
+
+
+
+# ----- Video transfer protocol -----
+
+
+def build_file_details(file_name, file_size):
+    return build_command(0, [file_name, file_size])
+
+
+def build_confirm_file(status):
+    return build_command(1, [status])
+
+
 def unpack(data):
     """Unpack a command string into opcode and parameters.
 
